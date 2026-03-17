e7d91f0e69232dd632f20e2312ceff4d9b7a5f24
zgredex <112968378+zgredex@users.noreply.github.com>
2026-03-17 20:45:14 +0100

    Update SettingsList.h

--- src/SettingsList.h
+++ src/SettingsList.h
@@ -75,7 +75,6 @@ inline const std::vector<SettingInfo>& getSettingsList() {
                         StrId::STR_CAT_CONTROLS),
 
       // --- System ---
-      // STR_NONE_OPT: serialized by JsonSettingsIO but UI ordering is handled in SettingsActivity
       SettingInfo::Enum(StrId::STR_TIME_TO_SLEEP, &CrossPointSettings::sleepTimeout,
                         {StrId::STR_MIN_1, StrId::STR_MIN_5, StrId::STR_MIN_10, StrId::STR_MIN_15, StrId::STR_MIN_30},
                         "sleepTimeout", StrId::STR_CAT_SYSTEM),
